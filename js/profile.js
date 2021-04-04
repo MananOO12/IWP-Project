af7271035c15e255d4e916341800f5cd918562aa
Manan Jain <manansj01@gmail.com>
2021-04-04 15:29:51 +0530

    modfied profile

--- js/profile.js
+++ js/profile.js
@@ -28,3 +28,49 @@ function PostComment1() {
   document.getElementById("txt1").style.display = "none";
   document.getElementById("bt2").style.display = "none";
 }
+
+// like button
+const likeBtn = document.querySelector(".like__btn");
+let likeIcon = document.querySelector("#icon"),
+  count = document.querySelector("#count");
+
+let clicked = false;
+
+
+likeBtn.addEventListener("click", () => {
+  if (!clicked) {
+    clicked = true;
+    likeIcon.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+    count.textContent++;
+  } else {
+    clicked = false;
+    likeIcon.innerHTML = `<i class="far fa-thumbs-up"></i>`;
+    count.textContent--;
+  }
+
+});
+
+const likeBtn2 = document.querySelector(".like__btn2");
+let likeIcon2 = document.querySelector("#icon2"),
+  count2 = document.querySelector("#count2");
+
+let clicked2 = false;
+
+likeBtn2.addEventListener("click", () => {
+  if (!clicked2) {
+    clicked2 = true;
+    likeIcon2.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+    count2.textContent++;
+  } else {
+    clicked2 = false;
+    likeIcon2.innerHTML = `<i class="far fa-thumbs-up"></i>`;
+    count2.textContent--;
+  }
+});
+
+//navbar
+var dd_main = document.querySelector(".dd_main");
+
+dd_main.addEventListener("click", function(){
+  this.classList.toggle("active");
+})
