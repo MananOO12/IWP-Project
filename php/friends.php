ba0f0e9075bb20884ae4feae0c0c1a5ad5423ae6
Manan Jain <manansj01@gmail.com>
2021-04-07 15:46:17 +0530

    "navbar link fixed"

--- php/friends.php
+++ php/friends.php
@@ -10,10 +10,10 @@
   <!-- bootstrap -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
 
   <link rel="stylesheet" href="../css/friends.css?v=<?php echo time();?>" />
-  <link rel="stylesheet" href="../css/profile-navbar.css?v=<?php echo time();?>" />
+  <link rel="stylesheet" href="../css/navbar.css?v=<?php echo time();?>" />
 
   <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
 
@@ -185,11 +185,11 @@
           </div>
         </div>
       </div>
-    
+
     </div>
     </div>
-  
-  
+
+
   <!-- jQuery library -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
